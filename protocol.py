d3a95f9bcbce01df852fdf687248b7e0f9d5a5c3
Mikaël Fourrier <mikael.fourrier@laposte.net>
2014-12-29 11:59:18 +0100

    str->int dans les messages parsés.

--- protocol.py
+++ protocol.py
@@ -109,19 +109,19 @@ def parse_register(message):
 def parse_init(message):
     """ Parse le message suivant le protocole donné.
 
-    >>> parse_init(EXAMPLE_INIT) == {'matchid':\
-            '20ac18ab-6d18-450e-94af-bee53fdc8fca', 'nb_players': '6', 'cells':\
-            [{'x': '23', 'y': '9', 'radius': '2', 'cellid': '1', 'offsize':\
-                '30', 'prod': 'I', 'defsize': '8'}, {'x': '41', 'y': '55',\
-                    'radius': '1', 'cellid': '2', 'offsize': '30', 'prod':\
-                    'II',\
-                    'defsize': '8'}, {'x': '23', 'y': '103', 'radius': '1',\
-                        'cellid': '3', 'offsize': '20', 'prod': 'I',\
-                        'defsize':\
-                        '5'}], 'speed': '1', 'id_us': '2', 'nb_lines': '2',\
-                    'nb_cells': '3', 'lines': [{'cellid2': '2', 'dist': '3433',\
-                        'cellid1': '1'}, {'cellid2': '3', 'dist': '6502',\
-                            'cellid1': '1'}]}
+    >>> parse_init(EXAMPLE_INIT) == \
+            {'matchid': '20ac18ab-6d18-450e-94af-bee53fdc8fca',\
+             'nb_players': 6,\
+             'cells': [{'x': 23, 'y': 9, 'radius': 2, 'cellid': 1,\
+                        'offsize': 30, 'prod': 'I', 'defsize': 8},\
+                       {'x': 41, 'y': 55, 'radius': 1, 'cellid': 2,\
+                        'offsize': 30, 'prod': 'II', 'defsize': 8},\
+                       {'x': 23, 'y': 103, 'radius': 1, 'cellid': 3,\
+                        'offsize': 20, 'prod': 'I', 'defsize': 5}],\
+             'speed': 1,\
+             'id_us': 2,\
+             'lines': [{'cellid2': 2, 'dist': 3433, 'cellid1': 1},\
+                       {'cellid2': 3, 'dist': 6502, 'cellid1': 1}]}
     True
     """
     LOGGER.debug('Reçu  :' + message)
@@ -134,6 +134,15 @@ def parse_init(message):
              for s in struct['lines'].split(',')]
     struct['cells'] = cells
     struct['lines'] = lines
+    # Suppression de champs inutiles
+    del struct['nb_cells'], struct['nb_lines']
+    # str -> int pour les champs numériques. la regex garrantit que ce sont des
+    # nombre.
+    _intify(struct, ['nb_players', 'speed', 'id_us'])
+    for cell in struct['cells']:
+        _intify(cell, ['x', 'y', 'radius', 'cellid', 'offsize', 'defsize'])
+    for line in struct['lines']:
+        _intify(line, ['cellid1', 'cellid2', 'dist'])
     LOGGER.debug('Parsé :' + str(struct))
     return struct
 
@@ -158,21 +167,18 @@ def parse_state(message):
     """ Parse le message suivant le protocole donné.
 
     >>> parse_state(EXAMPLE_STATE) == \
-                {'moves': [{'to': '1',\
-                   'owner': '2',\
-                   'from': '2',\
-                   'units': '5',\
-                   'timestamp': '232'},\
-                  {'to': '2', 'owner': '2', 'from': '1', 'units': '6', 'timestamp': '488'},\
-                  {'to': '2', 'owner': '1', 'from': '1', 'units': '3', 'timestamp': '4330'},\
-                  {'to': '1', 'owner': '1', 'from': '3', 'units': '10', 'timestamp': '2241'}],\
-                 'nb_cells': '3',\
-                 'nb_moves': '4',\
-                 'nb_players': '2',\
-                 'cells': [{'defunit': '4', 'owner': '2', 'offunit': '12', 'cellid': '1'},\
-                  {'defunit': '2', 'owner': '2', 'offunit': '15', 'cellid': '2'},\
-                  {'defunit': '6', 'owner': '1', 'offunit': '33', 'cellid': '3'}],\
-                  'matchid': '20ac18ab-6d18-450e-94af-bee53fdc8fca', 'type': 'state'}
+               {'moves': [\
+                   {'to': 1, 'owner': 2, 'from': 2, 'units': 5, 'timestamp': 232},\
+                   {'to': 2, 'owner': 2, 'from': 1, 'units': 6, 'timestamp': 488},\
+                   {'to': 2, 'owner': 1, 'from': 1, 'units': 3, 'timestamp': 4330},\
+                   {'to': 1, 'owner': 1, 'from': 3, 'units': 10, 'timestamp': 2241}],\
+                'cells': [\
+                    {'defunit': 4, 'owner': 2, 'offunit': 12, 'cellid': 1},\
+                    {'defunit': 2, 'owner': 2, 'offunit': 15, 'cellid': 2},\
+                    {'defunit': 6, 'owner': 1, 'offunit': 33, 'cellid': 3}],\
+                'nb_players': 2,\
+                'matchid': '20ac18ab-6d18-450e-94af-bee53fdc8fca',\
+                'type': 'state'}
     True
     """
     LOGGER.debug('Reçu  : ' + message)
@@ -186,6 +192,15 @@ def parse_state(message):
     struct['cells'] = cells
     struct['moves'] = moves
     struct['type'] = 'state'
+    # suppression de champs inutiles
+    del struct['nb_moves'], struct['nb_cells']
+    # str -> int pour les champs numériques. la regex garrantit que ce sont des
+    # nombre.
+    _intify(struct, ['nb_players'])
+    for cell in struct['cells']:
+        _intify(cell, ['defunit', 'owner', 'offunit', 'cellid'])
+    for move in struct['moves']:
+        _intify(move, ['to', 'owner', 'from', 'units', 'timestamp'])
     LOGGER.debug('Parsé : ' + str(struct))
     return struct
 
@@ -219,13 +234,14 @@ def encode_order(user_id, order):
 def parse_gameover(message):
     """ Parse un message de gameover.
 
-    >>> parse_gameover(EXAMPLE_GAMEOVER) == {'type': 'gameover', 'winner': '2',\
+    >>> parse_gameover(EXAMPLE_GAMEOVER) == {'type': 'gameover', 'winner': 2,\
             'matchid': '20ac18ab-6d18-450e-94af-bee53fdc8fca'}
     True
     """
     LOGGER.debug('Reçu  : ' + message)
     struct = REGEX_GAMEOVER.match(message).groupdict()
     struct['type'] = 'gameover'
+    _intify(struct, ['winner'])
     LOGGER.debug('Parsé : ' + str(struct))
     return struct
 
@@ -240,6 +256,16 @@ def parse_endofgame(message):
     struct = REGEX_ENDOFGAME.match(message).groupdict()
     struct['type'] = 'endofgame'
     LOGGER.debug('Parsé : ' + str(struct))
+    return struct
+
+def _intify(struct, keys):
+    """ Transforme les valeurs str en int.
+
+    >>> _intify({1: '1', 2: '2', 3: '3'}, [1, 3]) == {1: 1, 2: '2', 3: 3}
+    True
+    """
+    for key in keys:
+        struct[key] = int(struct[key])
     return struct
 
 
