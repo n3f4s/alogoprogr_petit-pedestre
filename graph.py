77febe4410cf4fc4ce05831e136485f0308ee141
Moripo <jeanlabat01@gmail.com>
2015-01-25 19:23:21 +0100

    Intro+conclu, et ajout d'une interface graphique simple

---
+++ graph.py
@@ -0,0 +1,16 @@
+from tkinter import *
+def graph(Canevas,match):
+    for cell in match.cells.values():
+        if cell.owner == match.me:
+            color = 'blue'
+        elif cell.owner == -1:
+            color = 'black'
+        else:
+            color = 'red'
+        x = cell.x/50 +40
+        y = cell.y/50 +40
+        r = cell.radius/15
+        Canevas.create_oval(x-r, y-r, x+r, y+r, outline=color, fill=color)
+        for id_ in cell.links:
+            Canevas.create_line(x,y,match.cells[id_].x/50+40,match.cells[id_].y/50+40)
+            
